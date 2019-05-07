e5476833c1da81a30f17e4f740de3af90453e923
Kevin <kevinlai76@gmail.com>
2019-05-07 22:11:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif str == str.upcase
     return "NO, NOT SINCE 1938!"
   else
